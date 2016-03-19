d3a356458b34396f021424e800d284e4e98091cb
Mickey <poverwhelming@gmail.com>
2016-03-19 23:51:55 +0800

    finished Exercise:ModulesAndFunctions-7

--- mm/do_this.exs
+++ mm/do_this.exs
@@ -8,4 +8,23 @@ defmodule DoThis do
     :io_lib.format("~.2f", [float])
   end
 
+  def print_env env_var do
+    env_var
+      |> String.upcase
+      |> System.get_env
+  end
+
+  def print_ext file_path do
+    Path.extname(file_path)
+  end
+
+  def current_dir do
+    IEx.Helpers.pwd
+  end
+
+  # Use poison
+
+  def run_cmd cmd, args \\ [], opts \\ [] do
+    System.cmd(cmd, args, opts)
+  end
 end
