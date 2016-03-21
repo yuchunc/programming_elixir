36840a572ad31708de515d1172344c759bb04b41
Mickey <poverwhelming@gmail.com>
2016-03-22 00:38:23 +0800

    finished all exercise 4 of list and recursion

--- list_and_recursion/exercise.exs
+++ list_and_recursion/exercise.exs
@@ -11,4 +11,17 @@ defmodule Exercise do
   def mapsum( [], _func), do: 0
 
   def mapsum( [head | tail], func ), do: func.(head) + mapsum(tail, func)
+
+  # Exercise 2
+  # NOTE Enum.max method is inplemented with reduce and Kernal.max
+  def list_max(list), do: Enum.max(list)
+
+  # Exercise 3
+  # NOTE ?z would produce the codepoint of the charter z, which is 122
+  def caesar([], _n), do: []
+
+  def caesar([head|tail], n) when head + n <= ?z, do: [ head + n | caesar(tail, n) ]
+
+  def caesar([head|tail], n), do: [ head + n - 26 | caesar(tail, n) ]
+
 end
